e3ddfe7b5d912f60c47965698979d4bbea58d999
anu singh <annusingh58@gmail.com>
2023-09-07 19:13:17 +0530

    updates

--- tfullstack/src/Global/Navbar.jsx
+++ tfullstack/src/Global/Navbar.jsx
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react'
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import {AuthContext} from '../Component/Context/AuthContext'
 
 const Navbar = () => {
-
     const {state,dispatch}=useContext(AuthContext); 
     const username = state?.user?.name;
     const router=useNavigate();
@@ -14,8 +13,7 @@ const Navbar = () => {
     }
 
   return (
-    
-        <div id="navbar">
+       <div id="navbar">
         <div id="nb">
             <div>             
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHIvMR55RXNIxhyJbtIomYn-IWTJng7WDhlQ&usqp=CAU"/>
@@ -28,83 +26,52 @@ const Navbar = () => {
             
         </div>
         {state?.user?.role==="seller" ?
-
+        
         <div id="nb3">
+            <div> <p>Add Product</p></div>
+            <div> <p>Update Product</p></div>
+            <div> <p>Delete Product</p></div>
+            
+
             <div>
                 <i class="fa-solid fa-store"></i>
-                <p>Add product</p>
-            </div>
-            {/* <div>
+                <p>STORES</p>
+            </div>:
+            <div>
                 <i class="fa-regular fa-user"></i>
                 {username?<p>{username}</p>:<p onClick={()=>router('/login')}>LOGIN</p>}
            
             </div>
+        }
+        
             <div id="switch">
             <div >
              {username?<button onClick={Logout}>LOGOUT</button>:<button></button>}
              <p onClick={()=>router('/register')}>Signup</p>
             </div>
-            </div> */}
+            </div>
 
             <div>
                 <i class="fa-regular fa-heart"></i>
-                <p>Update Product</p>
+                <p>WISHLIST</p>
             </div>
             <div>
-                <p>Delete Product</p>
-            </div>
-            {/* <div>
                 <i class="fa-solid fa-cart-shopping"></i>
                 <p onClick={()=>router('/cart')} >CART</p>
-            </div>  */}
-        </div>:
-         
-               
-                
-                <div id="nb3">
-                    <div>
-                        <i class="fa-solid fa-store"></i>
-                        <p>STORES</p>
-                    </div>
-                    <div>
-                        <i class="fa-regular fa-user"></i>
-                        <p>ACCOUNT</p>
-                    </div>
-                    <div>
-                        <i class="fa-regular fa-heart"></i>
-                        <p>WISHLIST</p>
-                    </div>
-                    <div>
-                        <i class="fa-solid fa-cart-shopping"></i>
-                        <p>CART</p>
-
-                    </div>
-                    
-
-                    <div>
-                <i class="fa-regular fa-user"></i>
-                {username?<p>{username}</p>:<p onClick={()=>router('/login')}>LOGIN</p>}
-           
+            </div> 
+            <div>
+                <i class="fa-solid fa-store"></i>
+                <p>STORES</p>
             </div>
-            <div id="switch">
-            <div >
-             {username?<button onClick={Logout}>LOGOUT</button>:<button></button>}
-             <p onClick={()=>router('/register')}>Signup</p>
-            </div>
-            </div>
-                </div>
-                </div>
-}
-
-
-
-            
-
+       
+        </div>
         
 
-    
-            
+   
   )
-}
+    
+
+    }
+
 
 export default Navbar
